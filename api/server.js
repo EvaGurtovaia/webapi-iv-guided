298aa2a6059477652493fa656880607d4086a0c2
EvaGurtovaia <eva.gurtovaya@gmail.com>
2019-06-12 22:57:34 -0500

    2

--- api/server.js
+++ api/server.js
@@ -1,33 +1,44 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const db = require('../data/db.js');
+const db = require("../data/db.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 
-server.get('/', async (req, res) => {
-  try {
-    const shoutouts = await db('shoutouts');
-    res.status(200).json(shoutouts);
-  } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot retrieve the shoutouts' });
-  }
+// server.get("/", async (req, res) => {
+//     try {
+//         const shoutouts = await db("shoutouts");
+//         res.status(200).json(shoutouts);
+//     } catch (error) {
+//         console.error("\nERROR", error);
+//         res.status(500).json({ error: "Cannot retrieve the shoutouts" });
+//     }
+// });
+
+server.post("/", async (req, res) => {
+    try {
+        const [id] = await db("shoutouts").insert(req.body);
+        const shoutouts = await db("shoutouts");
+
+        res.status(201).json(shoutouts);
+    } catch (error) {
+        console.error("\nERROR", error);
+        res.status(500).json({ error: "Cannot add the shoutout" });
+    }
 });
 
-server.post('/', async (req, res) => {
-  try {
-    const [id] = await db('shoutouts').insert(req.body);
-    const shoutouts = await db('shoutouts');
-
-    res.status(201).json(shoutouts);
-  } catch (error) {
-    console.error('\nERROR', error);
-    res.status(500).json({ error: 'Cannot add the shoutout' });
-  }
+server.get("/", async (req, res) => {
+    try {
+        const shoutouts = await db("shoutouts");
+        const messageOfTheDay = process.env.MOTD || "Hello World!"; // add this line
+        res.status(200).json({ motd: messageOfTheDay, shoutouts }); // change this line
+    } catch (error) {
+        console.error("\nERROR", error);
+        res.status(500).json({ error: "Cannot retrieve the shoutouts" });
+    }
 });
 
 module.exports = server;
